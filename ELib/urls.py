d20fa264e8849f3c8a3c19f09ca34d9ac10a0753
Lars Gordon <larichgo@web.de>
2024-09-22 14:53:34 +0200

    Updated urls and gitignore

--- ELib/urls.py
+++ ELib/urls.py
@@ -16,7 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    # We are only using a single app, so we redirect all urls there
+    path('', RedirectView.as_view(url='bibliothek/', permanent=True)),
     path('admin/', admin.site.urls),
+    path("bibliothek", include("bibliothek.urls")),
+
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
