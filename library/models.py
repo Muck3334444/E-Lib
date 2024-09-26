91a294d473272ad870d1a440b837e0ddeae06f21
Lars Gordon <larichgo@web.de>
2024-09-26 21:47:38 +0200

    Updated modells

--- library/models.py
+++ library/models.py
@@ -3,6 +3,7 @@ All Models concerning Books
 """
 
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Book(models.Model):
     """
@@ -13,6 +14,8 @@ class Book(models.Model):
     summary = models.TextField()
     author = models.ManyToManyField("Author")
     language = models.ManyToManyField("Language")
+    language = models.ManyToManyField("Genre")
+    price = models.FloatField()
 
     def __str__(self) -> str:
         return self.title
@@ -35,9 +38,28 @@ class Lease(models.Model):
     Describes what book is leased by whom, when and if it is returned
     """
     bookInstance = models.ForeignKey(BookInstance, on_delete=models.CASCADE, null=True)
-    details = models.CharField(max_length=1024, null=True)
     dueBack = models.DateField(null=True)
     returnDate = models.DateField(null=True)
+    leaseDate = models.DateTimeField(auto_now_add=True)
+
+class Rating(models.Model):
+    """
+    Describes a rating on the scale of 1 to 5 a user gave a specific book
+    """
+    rating = models.IntegerField(validators=[
+            MaxValueValidator(5), # Max allowed value is 5 stars
+            MinValueValidator(1) # Min is 1
+        ])
+    bookInstance = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+
+class Reservation(models.Model):
+    """
+    Describes the reservation of a specific instance of a book from a certain user
+    """
+    bookInstance = models.ForeignKey(BookInstance, on_delete=models.CASCADE)
+    creationDate = models.DateTimeField(auto_now_add=True)
+    isActive = models.BooleanField(default=False)
 
 
 class Language(models.Model):
@@ -55,7 +77,7 @@ class Author(models.Model):
     Describes an Author
     """
     name = models.CharField(max_length=256)
-    dateOfBirth = models.DateField()
+    dateOfBirth = models.DateField(null=True) # Even though every author has a birthdate it is not always known
     dateOfDeath = models.DateField(null=True)
 
     def __str__(self) -> str:
@@ -67,6 +89,7 @@ class Genre(models.Model):
     Describes the possible Genre
     """
     name = models.CharField(max_length=128)
+    description = models.CharField(max_length=512)
 
     def __str__(self) -> str:
         return self.name
