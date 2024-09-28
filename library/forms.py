69749e8787bfeda38225f0d3fb1bb68b019377ac
Lars Gordon <larichgo@web.de>
2024-09-28 18:21:05 +0200

    Updated create book to have a price

--- library/forms.py
+++ library/forms.py
@@ -4,9 +4,10 @@ from .models import Book
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = ['title', 'isbn', 'summary']
+        fields = ['title', 'isbn', 'price', 'summary']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'isbn': forms.TextInput(attrs={'class': 'form-control'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}),
             'summary': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
         }
